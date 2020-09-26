8a01739fe5f9909690d415e168b8ffc304ed2dd0
kevinmaiden7 <kevin.m.gallego@gmail.com>
2020-09-26 18:08:26 -0500

    Fixing things_Classic ML step

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -8,10 +8,10 @@ import string
 import nltk
 nltk.download('punkt')
 nltk.download('stopwords')
+#from nltk import word_tokenize
 from nltk.stem import SnowballStemmer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
-
 
 def max_length_text(df):
     max_length = 0
@@ -24,38 +24,41 @@ def text_normalization(data):
     data['text'] = data['text'].apply(lambda x: x.lower())
     data['text'] = data['text'].apply((lambda x: re.sub('[%s]' % re.escape(string.punctuation), '', x)))
 
-
 def remove_stop_words(data, language):
     stopwords = nltk.corpus.stopwords.words(language)
     for i in range(data.shape[0]):
         data.at[i, 'text'] = [word for word in nltk.word_tokenize(data.at[i, 'text']) if word not in stopwords]
         data.at[i, 'text'] = ' '.join(data.at[i, 'text'])
 
-
 def apply_stemming(data, language):
     stemmer = SnowballStemmer(language)
     for i in range(data.shape[0]):
          data.at[i, 'text'] = (' '.join([stemmer.stem(word) for word in data.at[i, 'text'].split()]))
 
+# get_matrix representation
 
-def get_matrix(data, representation, vocabulary_length, stop_words_flag, language):
+def get_matrix(data, representation, vocabulary_length, stemming, remove_stopwords, language):
 
     df = data.copy(deep = True)
     
     text_normalization(df) # Text normalization
     
     # Stop_words
-    if stop_words_flag:
+    if remove_stopwords:
         remove_stop_words(df, language)
     
-    apply_stemming(df, language) # Stemming
+    # Stemming
+    if stemming:
+        apply_stemming(df, language)
     
     # Word representation
     if representation == 'BoW':
+        #count_vectorizer = CountVectorizer(max_df = 0.9, max_features = vocabulary_length, min_df = 0.1)
         count_vectorizer = CountVectorizer(max_features = vocabulary_length)
         matrix = count_vectorizer.fit_transform(df.text)
         
     elif representation == 'tf-idf':
+        #tfidf_vectorizer = TfidfVectorizer(max_df = 0.9, max_features = vocabulary_length, min_df = 0.1, use_idf=True)
         tfidf_vectorizer = TfidfVectorizer(max_features = vocabulary_length, use_idf=True)
         matrix = tfidf_vectorizer.fit_transform(df.text)
     
